4b8ad9b9c21f4c3de072b805240193eead2f542a
niadavis <niadavislad@gmail.com>
2016-04-18 20:28:25 +0000

    add this

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -4,9 +4,9 @@ $(document).ready(function()
 
 	$("button").click(function(){
 		var postMessage = $("#message").val();
-		$("#wall").prepend("<div><img src='http://cdn.playbuzz.com/cdn/0079c830-3406-4c05-a5c1-bc43e8f01479/7dd84d70-768b-492b-88f7-a6c70f2db2e9.jpg' width='100'/>"+postMessage+"</div>");
+		$("#wall").prepend("<div><img src='http://cdn.playbuzz.com/cdn/0079c830-3406-4c05-a5c1-bc43e8f01479/7dd84d70-768b-492b-88f7-a6c70f2db2e9.jpg' width='100'/>"+postMessage+"<button>like</button> <span class='likes-count'>0</span> </div>");
 	});
-
+alert("0");
 	
 	// YOUR CODE GOES HERE
 });
